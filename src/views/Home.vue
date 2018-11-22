18b6de13b0b6e9a883ccaed824f06f56cb097570
Martin Rustler <martin.rustler@googlemail.com>
2018-11-22 11:22:53 +0100

    Initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,71 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container fill-height>
+    <v-layout align-center row wrap>
+      <v-flex v-for="(num, i) in doors" v-bind:key="i" xs2>
+        <Door :active="openDoor === i" :number="num" :col="i % 6" :row="Math.floor(i / 6)" :clickHandler="open" />
+      </v-flex>
+    </v-layout>
+    <v-btn dark fab small color="black" class="back" @click="toggleFullscreen">
+      <v-icon v-if="!fullscreen">mdi-fullscreen</v-icon>
+      <v-icon v-else>mdi-fullscreen-exit</v-icon>
+    </v-btn>
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Door from '../components/Door'
 
 export default {
-  name: 'home',
+  data () {
+    return {
+      fullscreen: false,
+      doorOpen: false,
+      doors: Array.from({ length: 24 }, (v, k) => k + 1)
+        .map(a => [Math.random(), a])
+        .sort((a, b) => a[0] - b[0])
+        .map(a => a[1]),
+      openDoor: undefined
+    }
+  },
   components: {
-    HelloWorld
+    Door
+  },
+  methods: {
+    toggleFullscreen: function () {
+      var elem = document.documentElement
+      if (!this.fullscreen) {
+        elem.requestFullscreen && elem.requestFullscreen()
+        elem.mozRequestFullScreen && elem.mozRequestFullScreen()
+        elem.webkitRequestFullscreen && elem.webkitRequestFullscreen()
+        elem.msRequestFullscreen && elem.msRequestFullscreen()
+      } else {
+        document.exitFullscreen && document.exitFullscreen()
+        document.mozCancelFullScreen && document.mozCancelFullScreen()
+        document.webkitExitFullscreen && document.webkitExitFullscreen()
+        document.msExitFullscreen && document.msExitFullscreen()
+      }
+      this.fullscreen = !this.fullscreen
+    },
+    open: function (door) {
+      this.openDoor = door === this.openDoor ? undefined : door
+    }
   }
 }
 </script>
+
+<style scoped>
+.container {
+  background-image: url('../assets/background.jpg');
+  background-size: 100% 100%;
+  max-width: none;
+  padding: 48px;
+}
+.back {
+  right: 5px;
+  top: 5px;
+  position: absolute;
+}
+.flex.xs2 {
+  height: calc((100vh - 96px) / 4);
+}
+</style>
